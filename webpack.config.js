434b0e3fa78b53afb9c608d1ca0de4c88957e813
archebaldo <zakharov90@gmail.com>
2024-02-27 19:50:08 +0300

    Add: env (development, production) for build mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
-module.exports = {
-  mode: `development`,
-  entry: path.resolve(__dirname, `src`, `index.js`),
-  output: {
-    filename: `[name].[contenthash:8].js`,
-    path: path.resolve(__dirname, `dist`),
-    clean: true,
-  },
+module.exports = (env) => {
+  const { mode } = env;
+
+  return {
+    mode: mode ?? `development`,
+    entry: path.resolve(__dirname, `src`, `index.js`),
+    output: {
+      filename: `[name].[contenthash:8].js`,
+      path: path.resolve(__dirname, `dist`),
+      clean: true,
+    },
+  };
 };
